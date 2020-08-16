0f637ec0cb75d9bd79462fdc900aea2b1b0d800e
rajdavid <47979488+rajdavid@users.noreply.github.com>
2020-08-16 16:26:45 +0530

    Tower Of Hanoi added Comments

--- Recursion/TowerOfHanoi.java
+++ Recursion/TowerOfHanoi.java
@@ -3,11 +3,15 @@ import java.util.Scanner;
 public class TowerOfHanoi {
 
     public static void main(String args[]) {
-	System.out.println("Enter number of Disc in tower of Hanoi");
-	Scanner sc=new Scanner(System.in);
+    
+    System.out.println("Enter number of Disc in tower of Hanoi");
+    
+    Scanner sc=new Scanner(System.in);
 	int noOfDisc=sc.nextInt();
-	sc.close();
-	towerOfHanoi(noOfDisc,'A','C','B');
+    
+    sc.close();
+    
+    towerOfHanoi(noOfDisc,'A','C','B');
 	
     }
     private static void towerOfHanoi(int noOfDisc, char fromRod, char toRod ,char auxilaryRod) {
@@ -17,9 +21,11 @@ public class TowerOfHanoi {
 		    return;
 	    }
 	    else {
+            // move n-1 disc from rod(A)  to auxiallary rod (C) using "to rod" (B) as aux
 		    towerOfHanoi(noOfDisc-1,fromRod,auxilaryRod,toRod);
 		    System.out.println("Moved disk " + noOfDisc + " from rod " +  fromRod + " to rod " + toRod);
-		    towerOfHanoi(noOfDisc-1,auxilaryRod,toRod,fromRod);
+            // move n-1 disc from auxiallary rod (C) to  "to rod" (B)  using (A) as aux
+            towerOfHanoi(noOfDisc-1,auxilaryRod,toRod,fromRod);
 	    }
 	
     }	
